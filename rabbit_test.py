a3744eaa92084cfc1464f0ce499a98a02360a0a3
balazskoncz <balazs.koncz.developer@gmail.com>
2020-02-07 21:58:24 +0100

    f
    xrgrt

--- rabbit_test.py
+++ rabbit_test.py
@@ -1,8 +1,8 @@
-import pika, os, urlparse
+import pika, os, urllib
 
 # Parse CLODUAMQP_URL (fallback to localhost)
 url_str = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost//')
-url = urlparse.urlparse(url_str)
+url = urllib.parse(url_str)
 params = pika.ConnectionParameters(host=url.hostname, virtual_host=url.path[1:],
     credentials=pika.PlainCredentials(url.username, url.password))
 
