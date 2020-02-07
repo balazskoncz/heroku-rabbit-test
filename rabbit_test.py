da1ce1a116c2058cd9ee6e634bed0aadb8f4d3cf
balazskoncz <balazs.koncz.developer@gmail.com>
2020-02-07 21:53:12 +0100

    Update rabbit_test.py
    wtf update

--- rabbit_test.py
+++ rabbit_test.py
@@ -11,11 +11,11 @@ channel = connection.channel() # start a channel
 channel.queue_declare(queue='hello') # Declare a queue
 # send a message
 channel.basic_publish(exchange='', routing_key='hello', body='Hello CloudAMQP!')
-print " [x] Sent 'Hello World!'"
+print(" [x] Sent 'Hello World!")
 
 # create a function which is called on incoming messages
 def callback(ch, method, properties, body):
-  print " [x] Received %r" % (body)
+  print(" [x] Received: {}".format(body))
 
 # set up subscription on the queue
 channel.basic_consume(callback,
