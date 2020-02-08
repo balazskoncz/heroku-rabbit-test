c4fac1df03f54ee0297e3e5887e1db884a0b3496
balazs.koncz <balazs.koncz.developer@gmail.com>
2020-02-08 11:55:04 +0100

    fixek és sender
    fixek

--- heroku_receiver.py
+++ heroku_receiver.py
@@ -4,22 +4,13 @@ import os
 import urllib
 print('Starting receiver')
 
-# hostname = 'fish.rmq.cloudamqp.com'
-# user = 'lftilnno'
-# passpword = '8wBK2bkZI-etTBvCgYZgiXBq8qwwbQ-T' 
-
-# credentials = pika.credentials.PlainCredentials(user, passpword)
-
-# print(hostname)
-# print(credentials)
-
 # connection = pika.BlockingConnection(
 #     pika.ConnectionParameters(host=hostname, credentials=credentials))
 
 url_str = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost//')
 url = urllib.parse.urlparse(url_str)
 
-print('total url: {url}'.format(url))
+print('total url: {}'.format(url_str))
 print('host: {}'.format(url.hostname))
 print('virtual host: {}'.format(url.path[1:]))
 
---
+++ local_sender.py
@@ -0,0 +1,21 @@
+import pika
+
+hostname = 'fish.rmq.cloudamqp.com'
+user = 'lftilnno'
+passpword = '8wBK2bkZI-etTBvCgYZgiXBq8qwwbQ-T' 
+
+credentials = pika.credentials.PlainCredentials(user, passpword)
+
+print(hostname)
+print(credentials)
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=hostname, credentials=credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello_r')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello_r',
+                      body='Greeting from localhost!!!!!!')
+print(" [x] Sent 'Hello World!'")
